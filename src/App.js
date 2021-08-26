cd60828aa777120a939aae50aa17208d53467c00
Rinaldo Lanza <rlanza@clip.ch>
2021-08-26 15:50:30 +0200

    project created first version

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import {useState} from "react";
+import './App.css';
+
+export default function App() {
+  const [counter, setCounter] = useState(0)
+  function buttonHandler() {
+    console.log("Button was clicked")
+  }
+  function counterPlus() {
+    setCounter(counter+1)
+    console.log(counter)
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello World
+        <button onClick={ buttonHandler }>click me</button>
+        <p>{counter}</p>
+        <button onClick={ counterPlus }>counterPlus</button>
+      </header>
+    </div>
+  )
+}
+
+
